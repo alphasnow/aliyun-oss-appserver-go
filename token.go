87d41b9ad3a1ea295d58b42207522de73af48c54
weifeng <wind91@foxmail.com>
2024-12-06 17:23:42 +0800

    test

--- token.go
+++ token.go
@@ -96,7 +96,7 @@ func (t *Token) Generate() (*SignatureToken, error) {
 
 	// callback
 	var callback string
-	if t.callback.isValid() {
+	if t.callback != nil && t.callback.isValid() {
 		var callbackStr []byte
 		callbackStr, err = json.Marshal(t.callback)
 		if err != nil {
